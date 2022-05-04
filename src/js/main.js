bf12832aa5329cf0abd77a849e2227a2c9119953
Jun Dev <jun.meathammer@gmail.com>
2022-05-04 09:43:24 +0300

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+(function ($) {
+    $(document).ready(function(){
+        $('.multiple-items').slick({
+            centerMode: true,
+            centerPadding: '550px',
+            slidesToShow: 1,
+            dots: true,
+            responsive: [
+            
+                {
+                    breakpoint: 1045,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '-60px',
+                        slidesToShow: 1
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 3
+                    }
+                },
+                    {
+                    breakpoint: 600,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
+            
+        });
+    });
+ 
+})(jQuery);
